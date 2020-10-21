f20b68ddd1c282bd183a96cc4a9e41b4aa862ea4
DaniNg98 <36421880+DaniNg98@users.noreply.github.com>
2020-10-21 15:24:15 +0200

    Oppgave 1: Hentet ut kildekode fra kompendiet

--- src/no/oslomet/cs/algdat/Eksamen/Main.java
+++ src/no/oslomet/cs/algdat/Eksamen/Main.java
@@ -7,6 +7,6 @@ public class Main {
     public static void main(String[] args) {
         EksamenSBinTre<String> tre = new EksamenSBinTre<>(Comparator.naturalOrder());
         System.out.println(tre.antall());
-        //test
+
     }
 }
